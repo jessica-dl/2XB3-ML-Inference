e27789bd8cd485ddd221198986118db4b4e83b63
jessica-dl <jessica.deleeuw2@gmail.com>
2019-03-26 20:46:28 -0400

    Working on post

--- ml_inference/__main__.py
+++ ml_inference/__main__.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return 'Hello, World!'
+@app.route('/upload', methods=['POST'])
+def process_image():
+    print(request.files)
+    # checking if the file is present or not.
+    if 'file' not in request.files:
+        return "No file found"
+
+    img = request.files['file']
+    new_img = age_face(img)
+    img_name = new_img[-10:]
+
+    try:
+        send_file(new_img, attachment_filename=img_name)
+    except Exception as e:
+        return str(e)
+
+
+def age_face(img):
+    return img
 
 
 app.run()
