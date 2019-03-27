fedb3bf3be8d848655c56486aa782d85c3f3e7dd
jessica-dl <jessica.deleeuw2@gmail.com>
2019-03-27 13:09:38 -0400

    Now able to receive and save image

--- ml_inference/__main__.py
+++ ml_inference/__main__.py
@@ -1,26 +1,36 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
+import cv2
+import numpy as np
+import os
+from werkzeug import secure_filename
+from io import StringIO, BytesIO
 
 app = Flask(__name__)
 
 
 @app.route('/infer', methods=['POST'])
 def process_image():
-    print(request.files)
     # checking if the file is present or not.
     if 'file' not in request.files:
         return "No file found"
+    file = request.files.get('file')
+    file_str = file.read()
 
-    def age_face(img):
-        return img
+    nparr = np.fromstring(file_str, np.uint8)
+    img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)  # cv2.IMREAD_COLOR in OpenCV 3.1
 
-    img = request.files['file']
-    new_img = age_face(img)
-    img_name = new_img[-10:]
+    # filename = secure_filename(file.filename)
+    # cv2.imwrite(os.path.join('C:\\Users\\Jessica\\Documents\\', filename), img_np)
+    img_file = StringIO()
+    img_file.write(cv2.imencode('.png', img_np)[1].tobytes())
 
-    try:
-        send_file(new_img, attachment_filename=img_name)
-    except Exception as e:
-        return str(e)
+    return send_file(
+        StringIO(cv2.imencode('.png', img_np)[1].tostring()),
+        mimetype='image/png',
+        as_attachment=True,
+        attachment_filename='response.png')
+
+    # return send_file(cv2.imencode('.png', img_np)[1].tobytes(), mimetype='image/png')
 
 
 @app.route('/', methods=['GET'])
@@ -29,4 +39,5 @@ def get_request():
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
